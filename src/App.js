8fe239db7d80c61a6d67c2df4a285895c56f2a09
monoranjan.das <monoranjan.das@covianalytics.com>
2021-09-23 03:02:21 +0600

    ractjs project frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+
+  const display = () => {
+    console.log(name + company);
+  }
+
+  const addData = () => {
+    console.log(name)
+      axios.post('http://localhost:3001/insert', {
+        name,
+        company
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      console.log(name)
+    }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Covi Analytics </p>
+
+        <div>
+          <label>Name : </label>
+          <input type="text" onChange={(event)=>{setName(event.target.value)}}></input>
+        </div>
+        <div>
+          <label>Company : </label>
+          <input type="text" onChange={(event)=> {setCompany(event.target.value)}}></input>
+        </div>
+        <button onClick={display}>Show Data</button>
+        <button onClick={addData}>Send Data</button>
+
       </header>
     </div>
   );
