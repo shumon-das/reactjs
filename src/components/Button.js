859076870ab93840ef4eb009e92047b515e63c38
monoranjan.das <monoranjan.das@covianalytics.com>
2021-09-23 12:22:46 +0600

    add button, event, propTyps, reactive style

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
+    return <button onClick={onClick} className="btn" style={{backgroundColor:color}}>{text}</button>
+}
+
+Button.defaultProps = {
+    text : "Add",
+    color : "blue"
+}
+Button.propTypes = {
+    text : PropTypes.string,
+    color : PropTypes.string
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
+
 
 const Header = ({title}) => {
+    const onClickFunc = () => {
+        console.log("button Clicked")
+    }
     return (
-        <div>
-            <header>
+        <div className='headerPage'>
+            <header className="header">
                 <h1>First Tasks {title}</h1>
+                  <Button color="green" text="Add" onClick={onClickFunc}/>
+                  <Button color="blue" text="Remove"/>
+                  <Button color="red" text="Subcribe"/>
             </header>
         </div>
     )
 }
 
 Header.defaultProps = {
-    title : "First practis on react"
+    title : "On React"
 }
 Header.propTypes = {
     title : PropTypes.string.isRequired
